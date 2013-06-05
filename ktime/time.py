cc30bfd9f7d67b995728791df539a1cc79e4d84f
Andrew Thompson <andrewkt@aktzero.com>
2013-06-05 01:22:31 -0400

    man that was ugly

--- ktime/time.py
+++ ktime/time.py
@@ -4,11 +4,12 @@ def gmtime(secs=None):
         return epoch
     result = list(epoch)
     remaining = secs
-    leap_year_secs = 31622400
+    LEAP_YEAR_SECS = 31622400
+    NORMAL_YEAR_SECS = 31536000
 
     def is_leap_year(year):
         if year % 4 == 0:
-            if year % 100 == 0:
+            if year % 100 == 0 and year >= 2000:
                 if year % 400 == 0:
                     return True
                 else: 
@@ -19,37 +20,33 @@ def gmtime(secs=None):
 
     def secs_this_year(year):
         if is_leap_year(year):
-            return leap_year_secs
+            return LEAP_YEAR_SECS
         else:
-            return 31536000
+            return NORMAL_YEAR_SECS
 
-    while ((remaining / leap_year_secs) >= 1):
-        remaining = remaining - secs_this_year(result[0])
+    while ((remaining / NORMAL_YEAR_SECS) >= 1):
+        remaining -= secs_this_year(result[0])
         result[0] += 1
 
     days = [None, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     def secs_this_month(year, month):
         if is_leap_year(year):
-            days[1] = 29
+            days[2] = 29
         secs_this_month = 24 * 60 * 60 * days[month]
         return secs_this_month
 
-    def secs_this_day(year, month, day):
-        if is_leap_year(year):
-            days[1] = 29
-        assert (day-1) <= days[month]
-
-        secs_this_day = 24 * 60 * 60
-        return secs_this_day
+    SECS_THIS_DAY = 86400
 
     while remaining / secs_this_month(result[0], result[1]) >= 1:
-        remaining = remaining - secs_this_month(result[0], result[1])
+        remaining -= secs_this_month(result[0], result[1])
         result[7] += days[result[1]]
         result[1] += 1
+    print result, remaining, secs_this_month(result[0], result[1]), is_leap_year(result[0])
 
-    while remaining / secs_this_day(result[0], result[1], result[2]) >= 1:
-        remaining = remaining - secs_this_day(result[0], result[1], result[2])
+    while remaining / SECS_THIS_DAY >= 1:
+        print result, remaining, SECS_THIS_DAY
+        remaining -= SECS_THIS_DAY
         result[2] += 1
         result[7] += 1
 
